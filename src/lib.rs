4a35e13f74ce0aa4e3b5dbdfc437dec2667afbe2
Colin Adler <colin@coder.com>
2020-08-05 01:04:26 -0500

    Update docs to indicate asynchronous design

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! ![crates.io](https://img.shields.io/crates/v/coder.svg) ![docs.rs](https://docs.rs/coder/badge.svg)
 //!
-//! A pure Rust wrapper around the [Coder](https://coder.com) On-Prem API.
+//! An asynchronous, pure Rust wrapper around the [Coder](https://coder.com) On-Prem API.
 //!
 //! ## Installation
 //!
