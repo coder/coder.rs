f71e8e7237ad2cb49fc6b81be2f21ae08e7ded42
Colin Adler <colin@coder.com>
2020-08-07 01:02:07 -0500

    Add user tests

--- src/builders/users/get.rs
+++ src/builders/users/get.rs
@@ -24,3 +24,46 @@ impl_builder!(
         /// Queries a user by their id.
         => get [] -> User = id,
 );
+
+#[cfg(test)]
+mod test {
+    use crate::client::test::{client, ids::*};
+    use crate::client::Executor;
+
+    #[tokio::test]
+    async fn test_user() {
+        let c = client();
+
+        let res = c
+            .users()
+            .get(USER_ID)
+            .execute()
+            .await
+            .expect("send request")
+            .response
+            .expect("api error returned");
+
+        // id shouldn't be empty
+        assert_ne!(res.id, "");
+    }
+
+    #[tokio::test]
+    async fn test_users() {
+        let c = client();
+
+        let res = c
+            .users()
+            .execute()
+            .await
+            .expect("send request")
+            .response
+            .expect("api error returned");
+
+        // we should get at least 1
+        assert_ne!(res.len(), 0);
+
+        // they should all have non-empty ids
+        let ok = res.iter().fold(true, |ok, usr| ok && usr.id != "");
+        assert_eq!(ok, true);
+    }
+}
